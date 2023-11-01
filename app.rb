9b451bd672f6bb3272e4071fd18557f4ea58fb02
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-11-01 00:55:49 +0000

    initial pmt page commit

--- app.rb
+++ app.rb
@@ -30,3 +30,22 @@ get("/square_root/results") do
 
   erb(:square_root_results)
 end
+
+#Beginning of payment page logic
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  @user_APR_input = params.fetch("user_APR").to_f
+  @user_Years_input = params.fetch("user_Years").to_f
+  @user_Principal_input = params.fetch("user_Principal").to_f
+
+  pmt_num = (@user_APR_input/12)*@user_Principal_input
+  pmt_denom = 1-((1+@user_APR_input/12)**-(@user_Years_input*12))
+
+  @user_payment_result = pmt_num/pmt_denom
+
+  erb(:payment_results)
+end
